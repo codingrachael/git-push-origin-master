1218c6c208efe24deed145bb9a88813e33e26bc5
Rachael McNamara <rachaelmcnamaraa@gmail.com>
2022-04-07 15:33:37 +1000

    Add dotenv - Protect my secret data in .env file

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,23 +5,25 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark = Bookmark.find(params[:id])
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     @bookmark.save
     if @bookmark.save
-      redirect_to list_path(@bookmark)
+      redirect_to list_path(@list)
     else
-    render :new
+      render :new
+    end
   end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path
+    redirect_to lists_path
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
